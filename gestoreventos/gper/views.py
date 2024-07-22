7e06dcb0a9bfd2ee2b61e707b809dc957a1ab0d3
Guillermo Galindo <31331135+DryMemoG@users.noreply.github.com>
2024-07-22 16:39:49 -0600

    Urls en carpeta

--- gestoreventos/gper/views.py
+++ gestoreventos/gper/views.py
@@ -94,27 +94,27 @@ class TecnicaCoccionDeleteView(DeleteView):
 #region CRUD Origen
 class OrigenListView(ListView):
     model = Origen
-    template_name = 'Origenes.html'
+    template_name = 'Origen/Origenes.html'
 
 class OrigenDetailView(DetailView):
     model = Origen
-    template_name = "OrigenDetalle.html"
+    template_name = "Origen/OrigenDetalle.html"
 
 class OrigenCreateView(CreateView):
     model = Origen
     form_class = OrigenForm
-    template_name = 'OrigenForm.html'
+    template_name = 'Origen/OrigenForm.html'
     success_url = reverse_lazy("OrigenList")
 
 class OrigenUpdateView(UpdateView):
     model = Origen
     form_class = OrigenForm
-    template_name = 'OrigenForm.html'
+    template_name = 'Origen/OrigenForm.html'
     success_url = reverse_lazy("OrigenList")
 
 class OrigenDeleteView(DeleteView):
     model = Origen
-    template_name = 'OrigenDeleteForm.html'
+    template_name = 'Origen/OrigenDeleteForm.html'
     success_url = reverse_lazy("OrigenList")
 
 #endregion
@@ -122,268 +122,268 @@ class OrigenDeleteView(DeleteView):
 
 class MenuListView(ListView):
     model = Menu
-    template_name = 'Menus.html'
+    template_name = 'Menu/Menus.html'
 
 class MenuDetailView(DetailView):
     model = Menu
-    template_name = "MenuDetalle.html"
+    template_name = "Menu/MenuDetalle.html"
 
 class MenuCreateView(CreateView):
     model = Menu
     form_class = MenuForm
-    template_name = 'MenuForm.html'
+    template_name = 'Menu/MenuForm.html'
     success_url = reverse_lazy("MenuList")
 
 class MenuUpdateView(UpdateView):
     model = Menu
     form_class = MenuForm
-    template_name = 'MenuForm.html'
+    template_name = 'Menu/MenuForm.html'
     success_url = reverse_lazy("MenuList")
 
 class MenuDeleteView(DeleteView):
     model = Menu
-    template_name = 'MenuDeleteForm.html'
+    template_name = 'Menu/MenuDeleteForm.html'
     success_url = reverse_lazy("MenuList")
 #endregion
 
 #region CRUD Receta
 class RecetaListView(ListView):
     model = Receta
-    template_name = 'Recetas.html'
+    template_name = 'Receta/Recetas.html'
 
 class RecetaDetailView(DetailView):
     model = Receta
-    template_name = "RecetaDetalle.html"
+    template_name = "Receta/RecetaDetalle.html"
 
 class RecetaCreateView(CreateView):
     model = Receta
     form_class = RecetaForm
-    template_name = 'RecetaForm.html'
+    template_name = 'Receta/RecetaForm.html'
     success_url = reverse_lazy("RecetaList")
 
 class RecetaUpdateView(UpdateView):
     model = Receta
     form_class = RecetaForm
-    template_name = 'RecetaForm.html'
+    template_name = 'Receta/RecetaForm.html'
     success_url = reverse_lazy("RecetaList")
 
 class RecetaDeleteView(DeleteView):
     model = Receta
-    template_name = 'RecetaDeleteForm.html'
+    template_name = 'Receta/RecetaDeleteForm.html'
     success_url = reverse_lazy("RecetaList")
 #endregion
 #region CRUD TecnicasReceta
 class TecnicasRecetaListView(ListView):
     model = TecnicasReceta
-    template_name = 'TecnicasRecetas.html'
+    template_name = 'TecnicasReceta/TecnicasRecetas.html'
 
 class TecnicasRecetaDetailView(DetailView):
     model = TecnicasReceta
-    template_name = "TecnicasRecetaDetalle.html"
+    template_name = "TecnicasReceta/TecnicasRecetaDetalle.html"
 
 class TecnicasRecetaCreateView(CreateView):
     model = TecnicasReceta
     form_class = TecnicasRecetaForm
-    template_name = 'TecnicasRecetaForm.html'
+    template_name = 'TecnicasReceta/TecnicasRecetaForm.html'
     success_url = reverse_lazy("TecnicasRecetaList")
 
 class TecnicasRecetaUpdateView(UpdateView):
     model = TecnicasReceta
     form_class = TecnicasRecetaForm
-    template_name = 'TecnicasRecetaForm.html'
+    template_name = 'TecnicasReceta/TecnicasRecetaForm.html'
     success_url = reverse_lazy("TecnicasRecetaList")
 
 class TecnicasRecetaDeleteView(DeleteView):
     model = TecnicasReceta
-    template_name = 'TecnicasRecetaDeleteForm.html'
+    template_name = 'TecnicasReceta/TecnicasRecetaDeleteForm.html'
     success_url = reverse_lazy("TecnicasRecetaList")
 #endregion
 #region CRUD IngredientesReceta
 class IngredientesRecetaListView(ListView):
     model = IngredientesReceta
-    template_name = 'IngredientesRecetas.html'
+    template_name = 'IngredientesReceta/IngredientesRecetas.html'
 
 class IngredientesRecetaDetailView(DetailView):
     model = IngredientesReceta
-    template_name = "IngredientesRecetaDetalle.html"
+    template_name = "IngredientesReceta/IngredientesRecetaDetalle.html"
 
 class IngredientesRecetaCreateView(CreateView):
     model = IngredientesReceta
     form_class = IngredientesRecetaForm
-    template_name = 'IngredientesRecetaForm.html'
+    template_name = 'IngredientesReceta/IngredientesRecetaForm.html'
     success_url = reverse_lazy("IngredientesRecetaList")
 
 class IngredientesRecetaUpdateView(UpdateView):
     model = IngredientesReceta
     form_class = IngredientesRecetaForm
-    template_name = 'IngredientesRecetaForm.html'
+    template_name = 'IngredientesReceta/IngredientesRecetaForm.html'
     success_url = reverse_lazy("IngredientesRecetaList")
 
 class IngredientesRecetaDeleteView(DeleteView):
     model = IngredientesReceta
-    template_name = 'IngredientesRecetaDeleteForm.html'
+    template_name = 'IngredientesReceta/IngredientesRecetaDeleteForm.html'
     success_url = reverse_lazy("IngredientesRecetaList")
 #endregion
 
 #region CRUD RecetaCostos
 class RecetaCostosListView(ListView):
     model = RecetaCostos
-    template_name = 'RecetaCostos.html'
+    template_name = 'RecetaCostos/RecetaCostos.html'
 
 class RecetaCostosDetailView(DetailView):
     model = RecetaCostos
-    template_name = "RecetaCostosDetalle.html"
+    template_name = "RecetaCostos/RecetaCostosDetalle.html"
 
 class RecetaCostosCreateView(CreateView):
     model = RecetaCostos
     form_class = RecetaCostosForm
-    template_name = 'RecetaCostosForm.html'
+    template_name = 'RecetaCostos/RecetaCostosForm.html'
     success_url = reverse_lazy("RecetaCostosList")
 
 class RecetaCostosUpdateView(UpdateView):
     model = RecetaCostos
     form_class = RecetaCostosForm
-    template_name = 'RecetaCostosForm.html'
+    template_name = 'RecetaCostos/RecetaCostosForm.html'
     success_url = reverse_lazy("RecetaCostosList")
 
 class RecetaCostosDeleteView(DeleteView):
     model = RecetaCostos
-    template_name = 'RecetaCostosDeleteForm.html'
+    template_name = 'RecetaCostos/RecetaCostosDeleteForm.html'
     success_url = reverse_lazy("RecetaCostosList")
 #endregion
 
 #region CRUD MenuRecetas
 class MenuRecetasListView(ListView):
     model = MenuRecetas
-    template_name = 'MenuRecetas.html'
+    template_name = 'MenuRecetas/MenuRecetas.html'
 
 class MenuRecetasDetailView(DetailView):
     model = MenuRecetas
-    template_name = "MenuRecetasDetalle.html"
+    template_name = "MenuRecetas/MenuRecetasDetalle.html"
 
 class MenuRecetasCreateView(CreateView):
     model = MenuRecetas
     form_class = MenuRecetasForm
-    template_name = 'MenuRecetasForm.html'
+    template_name = 'MenuRecetas/MenuRecetasForm.html'
     success_url = reverse_lazy("MenuRecetasList")
 
 class MenuRecetasUpdateView(UpdateView):
     model = MenuRecetas
     form_class = MenuRecetasForm
-    template_name = 'MenuRecetasForm.html'
+    template_name = 'MenuRecetas/MenuRecetasForm.html'
     success_url = reverse_lazy("MenuRecetasList")
 
 class MenuRecetasDeleteView(DeleteView):
     model = MenuRecetas
-    template_name = 'MenuRecetasDeleteForm.html'
+    template_name = 'MenuRecetas/MenuRecetasDeleteForm.html'
     success_url = reverse_lazy("MenuRecetasList")
 #endregion
 
 #region CRUD Cliente
 class ClienteListView(ListView):
     model = Cliente
-    template_name = 'Clientes.html'
+    template_name = 'Cliente/Clientes.html'
 
 class ClienteDetailView(DetailView):
     model = Cliente
-    template_name = "ClienteDetalle.html"
+    template_name = "Cliente/ClienteDetalle.html"
 
 class ClienteCreateView(CreateView):
     model = Cliente
     form_class = ClienteForm
-    template_name = 'ClienteForm.html'
+    template_name = 'Cliente/ClienteForm.html'
     success_url = reverse_lazy("ClienteList")
 
 class ClienteUpdateView(UpdateView):
     model = Cliente
     form_class = ClienteForm
-    template_name = 'ClienteForm.html'
+    template_name = 'Cliente/ClienteForm.html'
     success_url = reverse_lazy("ClienteList")
 
 class ClienteDeleteView(DeleteView):
     model = Cliente
-    template_name = 'ClienteDeleteForm.html'
+    template_name = 'Cliente/ClienteDeleteForm.html'
     success_url = reverse_lazy("ClienteList")
 #endregion
 
 #region CRUD ServiciosOpcionales
 class ServiciosOpcionalesListView(ListView):
     model = ServiciosOpcionales
-    template_name = 'ServiciosOpcionales.html'
+    template_name = 'ServiciosOpcionales/ServiciosOpcionales.html'
 
 class ServiciosOpcionalesDetailView(DetailView):
     model = ServiciosOpcionales
-    template_name = "ServiciosOpcionalesDetalle.html"
+    template_name = "ServiciosOpcionales/ServiciosOpcionalesDetalle.html"
 
 class ServiciosOpcionalesCreateView(CreateView):
     model = ServiciosOpcionales
     form_class = ServiciosOpcionalesForm
-    template_name = 'ServiciosOpcionalesForm.html'
+    template_name = 'ServiciosOpcionales/ServiciosOpcionalesForm.html'
     success_url = reverse_lazy("ServiciosOpcionalesList")
 
 class ServiciosOpcionalesUpdateView(UpdateView):
     model = ServiciosOpcionales
     form_class = ServiciosOpcionalesForm
-    template_name = 'ServiciosOpcionalesForm.html'
+    template_name = 'ServiciosOpcionales/ServiciosOpcionalesForm.html'
     success_url = reverse_lazy("ServiciosOpcionalesList")
 
 class ServiciosOpcionalesDeleteView(DeleteView):
     model = ServiciosOpcionales
-    template_name = 'ServiciosOpcionalesDeleteForm.html'
+    template_name = 'ServiciosOpcionales/ServiciosOpcionalesDeleteForm.html'
     success_url = reverse_lazy("ServiciosOpcionalesList")
 #endregion
 
 #region CRUD Lugar
 class LugarListView(ListView):
     model = Lugar
-    template_name = 'Lugares.html'
+    template_name = 'Lugar/Lugares.html'
 
 class LugarDetailView(DetailView):
     model = Lugar
-    template_name = "LugarDetalle.html"
+    template_name = "Lugar/LugarDetalle.html"
 
 class LugarCreateView(CreateView):
     model = Lugar
     form_class = LugarForm
-    template_name = 'LugarForm.html'
+    template_name = 'Lugar/LugarForm.html'
     success_url = reverse_lazy("LugarList")
 
 class LugarUpdateView(UpdateView):
     model = Lugar
     form_class = LugarForm
-    template_name = 'LugarForm.html'
+    template_name = 'Lugar/LugarForm.html'
     success_url = reverse_lazy("LugarList")
 
 class LugarDeleteView(DeleteView):
     model = Lugar
-    template_name = 'LugarDeleteForm.html'
+    template_name = 'Lugar/LugarDeleteForm.html'
     success_url = reverse_lazy("LugarList")
 #endregion
 
 #region CRUD Evento
 class EventoListView(ListView):
     model = Evento
-    template_name = 'Eventos.html'
+    template_name = 'Evento/Eventos.html'
 
 class EventoDetailView(DetailView):
     model = Evento
-    template_name = "EventoDetalle.html"
+    template_name = "Evento/EventoDetalle.html"
 
 class EventoCreateView(CreateView):
     model = Evento
     form_class = EventoForm
-    template_name = 'EventoForm.html'
+    template_name = 'Evento/EventoForm.html'
     success_url = reverse_lazy("EventoList")
 
 class EventoUpdateView(UpdateView):
     model = Evento
     form_class = EventoForm
-    template_name = 'EventoForm.html'
+    template_name = 'Evento/EventoForm.html'
     success_url = reverse_lazy("EventoList")
 
 class EventoDeleteView(DeleteView):
     model = Evento
-    template_name = 'EventoDeleteForm.html'
+    template_name = 'Evento/EventoDeleteForm.html'
     success_url = reverse_lazy("EventoList")
 
 #endregion
@@ -391,54 +391,54 @@ class EventoDeleteView(DeleteView):
 #region CRUD ServiciosOpcionalesLista
 class ServiciosOpcionalesListaListView(ListView):
     model = ServiciosOpcionalesLista
-    template_name = 'ServiciosOpcionalesListas.html'
+    template_name = 'ServiciosOpcionalesLista/ServiciosOpcionalesListas.html'
 
 class ServiciosOpcionalesListaDetailView(DetailView):
     model = ServiciosOpcionalesLista
-    template_name = "ServiciosOpcionalesListaDetalle.html"
+    template_name = "ServiciosOpcionalesLista/ServiciosOpcionalesListaDetalle.html"
 
 class ServiciosOpcionalesListaCreateView(CreateView):
     model = ServiciosOpcionalesLista
     form_class = ServiciosOpcionalesListaForm
-    template_name = 'ServiciosOpcionalesListaForm.html'
+    template_name = 'ServiciosOpcionalesLista/ServiciosOpcionalesListaForm.html'
     success_url = reverse_lazy("ServiciosOpcionalesListaList")
 
 class ServiciosOpcionalesListaUpdateView(UpdateView):
     model = ServiciosOpcionalesLista
     form_class = ServiciosOpcionalesListaForm
-    template_name = 'ServiciosOpcionalesListaForm.html'
+    template_name = 'ServiciosOpcionalesLista/ServiciosOpcionalesListaForm.html'
     success_url = reverse_lazy("ServiciosOpcionalesListaList")
 
 class ServiciosOpcionalesListaDeleteView(DeleteView):
     model = ServiciosOpcionalesLista
-    template_name = 'ServiciosOpcionalesListaDeleteForm.html'
+    template_name = 'ServiciosOpcionalesLista/ServiciosOpcionalesListaDeleteForm.html'
     success_url = reverse_lazy("ServiciosOpcionalesListaList")
 #endregion
 
 #region CRUD ListadoMenus
 class ListadoMenusListView(ListView):
     model = ListadoMenus
-    template_name = 'ListadoMenus.html'
+    template_name = 'ListadoMenus/ListadoMenus.html'
 
 class ListadoMenusDetailView(DetailView):
     model = ListadoMenus
-    template_name = "ListadoMenusDetalle.html"
+    template_name = "ListadoMenus/ListadoMenusDetalle.html"
 
 class ListadoMenusCreateView(CreateView):
     model = ListadoMenus
     form_class = ListadoMenusForm
-    template_name = 'ListadoMenusForm.html'
+    template_name = 'ListadoMenus/ListadoMenusForm.html'
     success_url = reverse_lazy("ListadoMenusList")
 
 class ListadoMenusUpdateView(UpdateView):
     model = ListadoMenus
     form_class = ListadoMenusForm
-    template_name = 'ListadoMenusForm.html'
+    template_name = 'ListadoMenus/ListadoMenusForm.html'
     success_url = reverse_lazy("ListadoMenusList")
 
 class ListadoMenusDeleteView(DeleteView):
     model = ListadoMenus
-    template_name = 'ListadoMenusDeleteForm.html'
+    template_name = 'ListadoMenus/ListadoMenusDeleteForm.html'
     success_url = reverse_lazy("ListadoMenusList")
 #endregion
 
